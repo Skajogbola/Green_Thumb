68909c6d2ff836b7a10f7b5c0414dbd89c11f629
Simmie <skajogbola@users.noreply.github.com>
2019-07-06 12:31:42 -0400

    Created the home page

---
+++ client/src/pages/PageOne.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Col, Row, Container } from "../components/Grid";
+
+
+function PageOne() {
+  return (
+    <Container fluid>
+      <Row>
+        <Col size="md-12">
+            <div className="card text-center">
+              <div className="card-header">
+                Featured
+ </div>
+              <div className="card-body">
+                <h5 className="card-title">Welcome to Green Thumb</h5>
+                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="google.com" className="btn btn-primary">LOGIN</a>
+              </div>
+              <div className="card-footer text-muted">
+                2 days ago
+ </div>
+            </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default PageOne;
