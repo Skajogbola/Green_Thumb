2a0dbaee21ec0edcf751abecd57a9bbcfa804c75
Nelly Mukami <yayalerato@yahoo.com>
2019-07-06 13:53:15 -0400

    homepage image

--- client/src/pages/PageOne.js
+++ client/src/pages/PageOne.js
@@ -7,19 +7,23 @@ function PageOne() {
     <Container fluid>
       <Row>
         <Col size="md-12">
-            <div className="card text-center">
-              <div className="card-header">
-                Featured
+          <div className="card text-center">
+            <div className="card-header">
+              Featured
  </div>
-              <div className="card-body">
-                <h5 className="card-title">Welcome to Green Thumb</h5>
-                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="google.com" className="btn btn-primary">LOGIN</a>
-              </div>
-              <div className="card-footer text-muted">
-                2 days ago
- </div>
+            <div className="card-body">
+              <img className="fit-picture"
+                src="../images/homepage.jpeg"
+                alt="Plants" />
+
+              <h5 className="card-title">Welcome to Green Thumb</h5>
+              <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="google.com" className="btn btn-primary">LOGIN</a>
             </div>
+            <div className="card-footer text-muted">
+              2 days ago
+ </div>
+          </div>
         </Col>
       </Row>
     </Container>
