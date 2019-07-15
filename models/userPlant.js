965b8e5c5456efbdb413ba4265bd1c355a60cb44
Simmie <skajogbola@users.noreply.github.com>
2019-07-15 17:05:47 -0400

    Working with database

--- models/userPlant.js
+++ models/userPlant.js
@@ -10,20 +10,20 @@ module.exports = function (sequelize, DataTypes) {
         },
     });
 
-    Post.associate = function (models) {
+    UserPlant.associate = function (models) {
         // We're saying that a UserPlant should belong to a User and a Plant
         // A UsePlant can't be created without a User and a Plant due to the foreign key constraint
-        Post.belongsTo(models.User, {
+        UserPlant.belongsTo(models.User, {
             foreignKey: {
                 allowNull: false
             }
         });
     };
 
-    Post.associate = function (models) {
+    UserPlant.associate = function (models) {
         // We're saying that a Post should belong to an Author
         // A Post can't be created without an Author due to the foreign key constraint
-        Post.belongsTo(models.Plant, {
+        UserPlant.belongsTo(models.Plant, {
             foreignKey: {
                 allowNull: false
             }
