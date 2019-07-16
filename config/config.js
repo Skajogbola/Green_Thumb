6f873375221807393b815afcad78286917fc1600
Simmie <skajogbola@users.noreply.github.com>
2019-07-16 19:25:22 -0400

    Working with heroku

--- config/config.js
+++ config/config.js
@@ -1,46 +1,46 @@
 require("dotenv").config();
-// module.exports = {
-//     "development": {
-//       "username": process.env.HEROKUUN,
-//       "password": process.env.HEROKUPW,
-//       "database": process.env.HEROKUDB,
-//       "host": process.env.HEROKUHOST,
-//       "dialect": "mysql"
-//     },
-//     "test": {
-//       "username": "root",
-//       "password": null,
-//       "database": "testdb",
-//       "host": "localhost",
-//       "dialect": "mysql",
-//       "logging": false
-//     },
-//     "production": {
-//       "use_env_variable": "JAWSDB_URL",
-//       "dialect": "mysql"
-//     }
-//   };
+module.exports = {
+    "development": {
+      "username": process.env.HEROKUUN,
+      "password": process.env.HEROKUPW,
+      "database": process.env.HEROKUDB,
+      "host": process.env.HEROKUHOST,
+      "dialect": "mysql"
+    },
+    "test": {
+      "username": "root",
+      "password": null,
+      "database": "testdb",
+      "host": "localhost",
+      "dialect": "mysql",
+      "logging": false
+    },
+    "production": {
+      "use_env_variable": "JAWSDB_URL",
+      "dialect": "mysql"
+    }
+  };
 
 // This is for local branch testing only.
-module.exports = {
-  "development": {
-    "username": "root",
-    "password": process.env.PASSWORD,
-    "database": process.env.DATABASE,
-    "host": "localhost",
-    "dialect": "mysql"
+// module.exports = {
+//   "development": {
+//     "username": "root",
+//     "password": process.env.PASSWORD,
+//     "database": process.env.DATABASE,
+//     "host": "localhost",
+//     "dialect": "mysql"
     
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "testdb",
-    "host": "localhost",
-    "dialect": "mysql",
-    "logging": false
-  },
-  "production": {
-    "use_env_variable": "JAWSDB_URL",
-    "dialect": "mysql"
-  }
-}
+//   },
+//   "test": {
+//     "username": "root",
+//     "password": null,
+//     "database": "testdb",
+//     "host": "localhost",
+//     "dialect": "mysql",
+//     "logging": false
+//   },
+//   "production": {
+//     "use_env_variable": "JAWSDB_URL",
+//     "dialect": "mysql"
+//   }
+// }
