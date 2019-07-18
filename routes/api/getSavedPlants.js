d83527b370ec96e28b8f4ee6d8805421625b36a2
Simmie <skajogbola@users.noreply.github.com>
2019-07-17 21:50:24 -0400

    Updates to saved page

--- routes/api/getSavedPlants.js
+++ routes/api/getSavedPlants.js
@@ -3,24 +3,63 @@ var db = require("../../models");
 const router = require("express").Router();
 
 router.route("/")
-// router.route("/getSavedPlants")
+    // router.route("/getSavedPlants")
     .post(function (req, res) {
         console.log("hello from plants", req.body)
+        //db.UserPlant.findAll({attributes:['plantId', 'userId', 'id']}).then(data=>console.log(data))
 
-        db.User.findAll({
-            where: { id: req.body.userId }, include: [
-                {
-                    model: db.UserPlant,
-                    where: {
-                        id: req.body.userId,
+        // Get all the plants
+        db.Plant.findAll()
+            .then(allPlants => {
+                //res.json(allPlants);
+                db.UserPlant
+                    .findAll(
+                        {
+                            attributes:['plantId', 'userId', 'id'],
+                            where:{userId: req.body.userId}
+                        }
+                    )
+                    .then(userPlantsIds => {
+                        let plantIds = [];
+                        userPlantsIds.forEach(e => {
+                            plantIds.push(e.plantId);
+                        });
+                        //res.json(plantIds);
+                        let updatedPlantIds = [...new Set(plantIds)];
+                        let userSavedPlants = [];
+                        allPlants.forEach( e => {
+                            if(updatedPlantIds.indexOf(e.id) !== -1) {
+                                userSavedPlants.push(e);
+                            }
+                        });
+                        res.status(200).json(userSavedPlants);
+                    })
+                    .catch(err => { console.log('Unable to get User Plants Ids', err) });
+            })
+            .catch(err => { console.log('Unabled to get all plants', err) });
 
-                    },
-                    required: true
-                }
-            ]
-        }).then(results => {
-            res.json(results)
-        })
+
+
+        //     where: { id: req.body.userId }, include: [
+        //         {
+        //             model: db.UserPlant,
+        //             where: {
+        //                 id: req.body.userId,
+        //                 include: [
+        //                     {
+        //                         model: db.Plant,
+        //                         where: {
+        //                             id: req.body.plantId,
+        //                         }
+        //                     }
+        //                 ]
+        //             },
+        //             required: true
+        //         }
+        //     ]
+        // }).then(results => {
+        //     res.json(results)
+        // })
     })
 
 
