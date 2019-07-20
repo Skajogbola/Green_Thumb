c52763b4310295675e6ac14e684fed74ba69c8b3
Simmie <skajogbola@users.noreply.github.com>
2019-07-20 00:01:08 -0400

    Final commit

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -2,12 +2,8 @@ import React, { Component } from "react";
 import Jumbotron from "../components/Jumbotron";
 import Card from "../components/Card";
 import API from "../utils/API";
-import browsebackgrd from "../images2/browsebkgrd.jpeg";
 import { Col, Row, Container } from "../components/Grid";
-// import Nav from "../components/Nav";
 import { Link } from "react-router-dom";
-
-
 
 
 class Saved extends Component {
@@ -16,22 +12,19 @@ class Saved extends Component {
     userInfo: {},
     userName: ""
   };
-
   componentDidMount() {
-    // console.log("hi")
+    console.log("hi")
     API.getSavedPlants().then(results => {
       console.log("Saved Plants", results)
-
       this.setState({
         savedPlants: results.data,
       }, () => console.log(this.state.savedPlants))
     }).catch(error => {
       console.log(error);
     })
-
     //get the value for the userinfo and set it as a variable if not null update the value from the string to an object
     var userInfo = localStorage.getItem("userinfo")
-    // console.log(userInfo)
+    console.log(userInfo)
     if (userInfo) {
       userInfo = JSON.parse(userInfo);
       this.setState({ userInfo, userName: userInfo.firstName }, () => {
@@ -45,12 +38,10 @@ class Saved extends Component {
         })
       })
     }
-
   }
-
+  
   render() {
     return (
-
       <Container>
         <Row>
           <Col size="md-12">
@@ -60,50 +51,38 @@ class Saved extends Component {
                 </Link>G R E E N    T H U M B</strong>
               </h1>
               <h2 className="text-center">Welcome to your saved plants {this.state.userName}</h2>
-
             </Jumbotron>
           </Col>
         </Row>
-
-
-      <div style={{ position: "relative", width:"100%", height:"100%", backgroundImage: `url(${browsebackgrd})` }}>
-        {/* <Nav></Nav> */}
-
-        <Container>
-          <Row>
-            <Col size="md-12">
-              <Jumbotron>
-                <h1 className="text-center">
-                  <strong>G R E E N    T H U M B</strong>
-                </h1>
-                <h2 className="text-center">Welcome to your saved plants {this.state.userName}</h2>
-              </Jumbotron>
-            </Col>
-          </Row>
-
-          <Row>
-            {
-              this.state.savedPlants.length > 0 &&
-              this.state.savedPlants.map(e =>
-                <Col key={e.id}
-                  size="md-6">
-                  <Card
-                    title={e.name}>
-                    <p>{e.careSummary}</p>
-                    <img className="img-fluid"
-                      style={{ maxHeight: "300px", maxWidth: "300px" }}
-                      src={e.picture} alt={e.id}
-                    />
-                  </Card>
-                </Col>
-              )
-            }
-          </Row>
-
-        </Container>
-      </div>
+        <Row>
+          {/* <Col size="md-6">
+            <Card> */}
+          {
+            this.state.savedPlants.length > 0 &&
+            this.state.savedPlants.map(e =>
+              // <div key={e.id}>
+              <Col key={e.id}
+                size="md-6">
+                <Card
+                  title={e.name}>
+                  <p>{e.careSummary}</p>
+                  {/* // <h3>{e.name}</h3>
+                    // <p>{e.careSummary}</p> */}
+                  <img className="img-fluid"
+                    // style={{ maxHeight: "300px", maxWidth: "300px" }}
+                    src={e.picture} alt={e.id}
+                  />
+                  {/* </div> */}
+                  {/* )
+              } */}
+                </Card>
+                {/* </Card> */}
+              </Col>
+            )
+          }
+        </Row>
+      </Container>
     );
   }
 }
-
 export default Saved;
